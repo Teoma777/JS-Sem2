f701489940fad1b3b20f33f3707ad7e17d132389
Teoma777 <groux.thomas.uniscite@gmail.com>
2019-05-20 14:16:07 +0200

    3

--- script.js
+++ script.js
@@ -15,3 +15,10 @@ var navBar = document.getElementById("navbarHeader");
 	burgerButton.addEventListener("click", function() {
 	navBar.classList.toggle("collapse");
 	 }); 
+
+var redEdit = document.getElementsByClassName("btn btn-sm btn-outline-secondary")[0];
+var redText = document.getElementsByClassName("card-text")[0];
+redEdit.addEventListener("click", function() {
+redText.style.color = "red";	
+	console.log("RED OK");
+});
