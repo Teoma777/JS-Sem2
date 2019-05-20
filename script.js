82509f9388c20dad9def6562303211ab601c459e
Teoma777 <groux.thomas.uniscite@gmail.com>
2019-05-20 17:12:53 +0200

    6 en cours

--- script.js
+++ script.js
@@ -40,14 +40,29 @@ var bsLink = document.querySelector("head > link:nth-child(2)");
 
 
 atomicBar.addEventListener("dblclick", function() {
-		console.log("ATOMIC");
+
+
 
 	if (bsLink.href = "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css") {
-			bsLink.href = ""
-	}else if (bsLink.href = ""){
+					console.log("ATOMIC");
 
+			bsLink.href = ""
+	}else if(bsLink.href = "file:///Users/a777/Desktop/CODE/THP/JAVA2/JS-Sem2/exo_events.html") {
+		console.log("XXX")
 		bsLink.href = "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
 	}
+});
+
+
+var card = document.getElementsByClassName("col-md-4")[0];
+var cardText = document.getElementsByClassName("card-text")[0];
+var picSize = document.getElementsByClassName("card-img-top")[0];
+
+card.addEventListener("mouseover", function() {
+	console.log("CA MARCHE");
+cardText.innerHTML = ""
+picSize.style.width = "20%" ;
+
 });
 
 
